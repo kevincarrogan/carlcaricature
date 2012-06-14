9c7b8e216e0d7d9aea81ca0553383f739d05fda2
Kevin Morgan <kevin.morgan@hogarthww.com>
2012-06-14 10:03:46 +0100

    Adding Twitter bot.

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+import tweepy
+import requests
+
+consumer_key = os.environ.get('consumer_key')
+consumer_secret = os.environ.get('consumer_secret')
+
+access_token = os.environ.get('access_token')
+access_token_secret = os.environ.get('access_token_secret')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+status_code = requests.get('http://www.unboxedconsulting.com/people/carl-whittaker').status_code
+status_response = 'YES' if status_code == 200 else 'NO'
+
+api.update_status(status_response)
